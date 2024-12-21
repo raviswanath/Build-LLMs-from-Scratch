42a5ba68768743ae82ec439861683df5af58ae29
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2024-12-20 20:45:57 -0500

    Update SelfAttentionv2.py

--- L3/SelfAttentionv2.py
+++ L3/SelfAttentionv2.py
@@ -19,15 +19,15 @@ class SelfAttentionv2(nn.Module):
         return context_vecs
 
 
-inputs = torch.tensor([
-    [0.43, 0.15, 0.89], 
-    [0.55, 0.87, 0.66], 
-    [0.57, 0.85, 0.64], 
-    [0.22, 0.58, 0.33], 
-    [0.77, 0.25, 0.10], 
-    [0.05, 0.80, 0.55]])
+# inputs = torch.tensor([
+#     [0.43, 0.15, 0.89], 
+#     [0.55, 0.87, 0.66], 
+#     [0.57, 0.85, 0.64], 
+#     [0.22, 0.58, 0.33], 
+#     [0.77, 0.25, 0.10], 
+#     [0.05, 0.80, 0.55]])
 
-d_in, d_out = 3, 2
-torch.manual_seed(789)
-sa = SelfAttentionv2(d_in, d_out)
-print(sa(inputs))
+# d_in, d_out = 3, 2
+# torch.manual_seed(789)
+# sa = SelfAttentionv2(d_in, d_out)
+# print(sa(inputs))
