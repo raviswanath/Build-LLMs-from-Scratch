1cbfe3dc93a7ded9560d3deb50a3b0627c05f53c
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2024-12-23 18:59:12 -0500

    Update MultiHeadedAttention.py

--- L3/MultiHeadedAttention.py
+++ L3/MultiHeadedAttention.py
@@ -46,18 +46,18 @@ class MultiHeadedAttention(nn.Module):
         return context_vec
 
 
-torch.manual_seed(123)
-inputs = torch.tensor([
-    [0.43, 0.15, 0.89], 
-    [0.55, 0.87, 0.66], 
-    [0.57, 0.85, 0.64], 
-    [0.22, 0.58, 0.33], 
-    [0.77, 0.25, 0.10], 
-    [0.05, 0.80, 0.55]])
+# torch.manual_seed(123)
+# inputs = torch.tensor([
+#     [0.43, 0.15, 0.89], 
+#     [0.55, 0.87, 0.66], 
+#     [0.57, 0.85, 0.64], 
+#     [0.22, 0.58, 0.33], 
+#     [0.77, 0.25, 0.10], 
+#     [0.05, 0.80, 0.55]])
 
-batch = torch.stack((inputs, inputs), dim=0)
-batch_size, context_length, d_in = batch.shape 
-d_out = 2
-mha = MultiHeadedAttention(d_in, d_out, context_length, 0.0, 2)
-context_vecs = mha(batch)
-print(context_vecs)
+# batch = torch.stack((inputs, inputs), dim=0)
+# batch_size, context_length, d_in = batch.shape 
+# d_out = 2
+# mha = MultiHeadedAttention(d_in, d_out, context_length, 0.0, 2)
+# context_vecs = mha(batch)
+# print(context_vecs)
