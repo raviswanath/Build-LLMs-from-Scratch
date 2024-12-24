741b7b8ed4011ed2152e0f5d0b71b3e645a928c7
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2024-12-23 19:21:06 -0500

    Create LayerNorm.py

---
+++ L4/LayerNorm.py
@@ -0,0 +1,15 @@
+import torch 
+
+class LayerNorm(torch.nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = torch.nn.Parameter(torch.ones(emb_dim))
+        self.shift = torch.nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps) 
+        return self.scale * norm_x + self.shift 
+    
