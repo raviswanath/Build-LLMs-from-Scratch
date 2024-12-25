4f5488593528dff7b8487c1f8e08ebede19011c1
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2024-12-25 16:43:38 -0500

    Update Transformer.py

--- L4/Transformer.py
+++ L4/Transformer.py
@@ -12,7 +12,7 @@ class TransformerBlock(nn.Module):
             d_in = cfg["emb_dim"], 
             d_out = cfg["emb_dim"], 
             context_length = cfg["context_length"],
-            num_heads = cfg["num_heads"],
+            num_heads = cfg["n_heads"],
             dropout = cfg["drop_rate"],
             qkv_bias = cfg["qkv_bias"]
         )
