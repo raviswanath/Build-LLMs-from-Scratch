39878cecf634131fb01d91c000fd90762854d6fa
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2024-12-27 15:07:34 -0500

    Update spacing

--- L4/Transformer.py
+++ L4/Transformer.py
@@ -9,12 +9,12 @@ class TransformerBlock(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.att = MultiHeadedAttention(
-            d_in = cfg["emb_dim"], 
-            d_out = cfg["emb_dim"], 
-            context_length = cfg["context_length"],
-            num_heads = cfg["n_heads"],
-            dropout = cfg["drop_rate"],
-            qkv_bias = cfg["qkv_bias"]
+            d_in=cfg["emb_dim"], 
+            d_out=cfg["emb_dim"], 
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"]
         )
         self.ff = FeedForward(cfg)
         self.norm1 = LayerNorm(cfg["emb_dim"])
