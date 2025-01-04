91ae3e08eaafdcce9744e58eaa8293f72d11dc17
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2025-01-03 21:07:18 -0500

    Update ModelEval.py

--- L5/ModelEval.py
+++ L5/ModelEval.py
@@ -90,7 +90,7 @@ def generate_and_print_sample(model, tokenizer, device, start_context):
     model.eval()
     context_size = model.pos_emb.weight.shape[0]
     encoded = text_to_token(start_context, tokenizer).to(device)
-    with torch.no_grad()
+    with torch.no_grad():
         token_ids = generate_text_simple(model, encoded, 50, context_size)
     decoded_text = token_to_text(token_ids, tokenizer)
     print(decoded_text.replace("\n", " "))
