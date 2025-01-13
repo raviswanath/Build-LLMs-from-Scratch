a8e57fae02d3ef36504ccbf1fdeab3bd9b28da2d
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2025-01-13 16:06:28 -0500

    Update LoadData_ClassificationFineTune.py

--- L6/LoadData_ClassificationFineTune.py
+++ L6/LoadData_ClassificationFineTune.py
@@ -3,6 +3,7 @@ import zipfile, os
 from pathlib import Path 
 import pandas as pd
 
+
 url = 'https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip'
 zip_path = "sms_spam_collection.zip"
 extracted_path = "sms_spam_collection"
@@ -29,9 +30,38 @@ def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
     print(f"File downloaded and saved at path {original_file_path}")
 
 
+def create_balanced_dataset(df):
+    num_spam = df[df["Label"] == "spam"].shape[0]
+    ham_subset = df[df["Label"] == "ham"].sample(
+        num_spam, random_state=123
+    )
+    balanced_df = pd.concat([ham_subset, df[df.Label == "spam"]])
+    return balanced_df
+
+
+def random_split(df, train_frac, val_frac):
+
+    # shuffle the dataset first
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    train_end = int(df.shape[0] * train_frac)
+    val_end = train_end + int(df.shape[0] * val_frac)
+
+    df_train = df.iloc[:train_end]
+    df_val = df.iloc[train_end:val_end]
+    df_test = df.iloc[val_end:]
+
+    return df_train, df_val, df_test
+
+
 download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
 
 df = pd.read_csv(data_file_path, sep="\t", header=None, names=["Label", "Text"])
-print(df.head())
+balanced_df = create_balanced_dataset(df)
+balanced_df["Label"] = balanced_df["Label"].map({"ham": 0, "spam": 1})
 
-print(df["Label"].value_counts())
+train_df, validation_df, test_df = random_split(balanced_df, 0.7, 0.1)
+
+print(train_df.shape)
+print(validation_df.shape)
+print(test_df.shape)
