ecbb9f88dfc84a320dd1324cd72a67492b5fed55
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2025-01-16 21:33:14 -0500

    Update GPT2-testing.py

--- L6/GPT2-testing.py
+++ L6/GPT2-testing.py
@@ -1,18 +1,16 @@
 import torch
+import os 
+import sys 
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+# Add the root directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from L6.SpamDataLoader import train_loader, val_loader, test_loader
 
 # Load the tokenizer and model
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 model = GPT2LMHeadModel.from_pretrained("gpt2")
-
-# Generate text
-input_text = "Every effort move you"
-input_ids = tokenizer.encode(input_text, return_tensors="pt")
-output = model.generate(input_ids)
-
-# Decode the output
-decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
-print(decoded_output)
+model.to(device)
 
 for param in model.parameters():
     param.requires_grad = False
@@ -30,24 +28,9 @@ for param in model.transformer.h[-1].parameters():
 for param in model.transformer.ln_f.parameters():
     param.requires_grad = True
 
-inputs = tokenizer.encode("Do you have time")
-inputs = torch.tensor(inputs).unsqueeze(0)
-print("Inputs:", inputs)
-print("Inputs dimensions:", inputs.shape)
-
-with torch.no_grad():
-    outputs = model(inputs)
-print("Outputs:\n", outputs.logits)
-print("Outputs dimensions:", outputs.logits.shape)
-print("Last output token:", outputs.logits[:, -1, :])
-
-
-probas = torch.softmax(outputs.logits[:, -1, :], dim=-1)
-label = torch.argmax(probas)
-print("Class label:", label.item())
-
 
 def calc_accuracy_loader(data_loader, model, device, num_batches=None):
+    model.to(device)
     model.eval()
     correct_predictions, num_examples = 0, 0
 
@@ -70,3 +53,12 @@ def calc_accuracy_loader(data_loader, model, device, num_batches=None):
         else:
             break
     return correct_predictions/num_examples
+
+
+train_accuracy = calc_accuracy_loader(train_loader, model, device=device, num_batches=10)
+val_accuracy = calc_accuracy_loader(val_loader, model, device=device, num_batches=10)
+test_accuracy = calc_accuracy_loader(test_loader, model, device=device, num_batches=10)
+
+print(f"Train accuracy {train_accuracy*100:.2f}%")
+print(f"Validation accuracy {val_accuracy*100:.2f}%")
+print(f"Test accuracy {test_accuracy*100:.2f}%")
