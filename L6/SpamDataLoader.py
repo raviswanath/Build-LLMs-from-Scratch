026189d02d18cde4a520889489b083e592b1b738
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2025-01-16 21:33:02 -0500

    Update SpamDataLoader.py

--- L6/SpamDataLoader.py
+++ L6/SpamDataLoader.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 from torch.utils.data import DataLoader
 
-# Add the root directory (parent of L4 and L5) to sys.path
+# Add the root directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from L6.PyTorchDataSet_Setup import SpamDataset
@@ -59,11 +59,11 @@ test_loader = DataLoader(
     drop_last=False
 )
 
-for input_batch, target_batch in train_loader:
-    pass
-print("Input batch dimensions:", input_batch.shape)
-print("Label batch dimensions", target_batch.shape)
+# for input_batch, target_batch in train_loader:
+#     pass
+# print("Input batch dimensions:", input_batch.shape)
+# print("Label batch dimensions", target_batch.shape)
 
-print(f"{len(train_loader)} training batches")
-print(f"{len(val_loader)} validation batches")
-print(f"{len(test_loader)} test batches")
+# print(f"{len(train_loader)} training batches")
+# print(f"{len(val_loader)} validation batches")
+# print(f"{len(test_loader)} test batches")
