765bfdbff8c63eea373e67f3e5559064b91c2e3d
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2025-01-18 18:52:53 -0500

    Update GPT2-testing.py

--- L6/GPT2-testing.py
+++ L6/GPT2-testing.py
@@ -1,10 +1,13 @@
 import torch
 import os 
 import sys 
+import time
+import numpy as np
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 # Add the root directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from L6.SpamDataLoader import train_loader, val_loader, test_loader
+from L6.GPT2_retraining_for_classification import train_classifier_simple, plot_values
 
 # Load the tokenizer and model
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
@@ -28,37 +31,36 @@ for param in model.transformer.h[-1].parameters():
 for param in model.transformer.ln_f.parameters():
     param.requires_grad = True
 
+start_time = time.time()
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5, weight_decay=0.1)
+num_epochs = 5
 
-def calc_accuracy_loader(data_loader, model, device, num_batches=None):
-    model.to(device)
-    model.eval()
-    correct_predictions, num_examples = 0, 0
+train_losses, val_losses, train_Accs, val_accs, ex_seen = train_classifier_simple(
+    model, train_loader, val_loader, optimizer, device, 
+    num_epochs, eval_freq=50, eval_iter=5
+)
 
-    if num_batches is None:
-        num_batches = len(data_loader)
-    else:
-        num_batches = min(num_batches, len(data_loader))
+end_time = time.time()
+execution_time = (end_time - start_time) / 60
+print(f"Training completed in {execution_time:.2f} minutes.")
 
-    for i, (input_batch, target_batch) in enumerate(data_loader):
-        if i < num_batches:
-            input_batch = input_batch.to(device)
-            target_batch = target_batch.to(device)
+epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+ex_seen_tensor = torch.linspace(0, ex_seen, len(train_losses))
 
-            with torch.no_grad():
-                logits = model(input_batch).logits[:, -1, :]
-            predicted_labels = torch.argmax(logits, dim=-1)
-
-            num_examples += predicted_labels.shape[0]
-            correct_predictions += (predicted_labels == target_batch).sum().item()
-        else:
-            break
-    return correct_predictions/num_examples
+train_losses = [i.item() for i in train_losses]
+val_losses = [i.item() for i in val_losses]
+plot_values(epochs_tensor, ex_seen_tensor, train_losses, val_losses)
 
 
-train_accuracy = calc_accuracy_loader(train_loader, model, device=device, num_batches=10)
-val_accuracy = calc_accuracy_loader(val_loader, model, device=device, num_batches=10)
-test_accuracy = calc_accuracy_loader(test_loader, model, device=device, num_batches=10)
+# with torch.no_grad():
+#     train_loss = calc_loader_loss(train_loader, model, device, num_batches=5)
+#     val_loss = calc_loader_loss(val_loader, model, device, num_batches=5)
+#     test_loss = calc_loader_loss(test_loader, model, device, num_batches=5)
 
-print(f"Train accuracy {train_accuracy*100:.2f}%")
-print(f"Validation accuracy {val_accuracy*100:.2f}%")
-print(f"Test accuracy {test_accuracy*100:.2f}%")
+# train_accuracy = calc_accuracy_loader(train_loader, model, device=device, num_batches=10)
+# val_accuracy = calc_accuracy_loader(val_loader, model, device=device, num_batches=10)
+# test_accuracy = calc_accuracy_loader(test_loader, model, device=device, num_batches=10)
+
+# print(f"Training Loss {train_loss:.2f}")
+# print(f"Validation Loss {val_loss:.2f}")
+# print(f"Test Loss {test_loss:.2f}")
