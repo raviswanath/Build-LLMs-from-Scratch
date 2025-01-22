3e26d93da7de046fe2570c0bc608297047f16026
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2025-01-21 20:45:02 -0500

    Update GPT2ClassificationRetraining_functions.py

--- L6/GPT2ClassificationRetraining_functions.py
+++ L6/GPT2ClassificationRetraining_functions.py
@@ -71,38 +71,40 @@ def evaluate_model(model, train_loader, val_loader, device, eval_iter):
     return train_loss, val_loss 
 
 
-def train_classifier_simple(model, train_loader, val_loader, optimizer, 
-                            device, num_epochs, eval_freq, eval_iter):
-    
+def train_classifier_simple(model, train_loader, val_loader, optimizer, device, num_epochs,
+                            eval_freq, eval_iter):
+    # Initialize lists to track losses and examples seen
     train_losses, val_losses, train_accs, val_accs = [], [], [], []
     examples_seen, global_step = 0, -1
 
+    # Main training loop
     for epoch in range(num_epochs):
-        model.train()
+        model.train()  # Set model to training mode
 
-        for input, target in train_loader:
-            optimizer.zero_grad()
-            loss = calc_batch_loss(input, target, model, device)
-            loss.backward()
-            optimizer.step()
-            examples_seen += input.shape[0]
-            global_step += 1 
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad() # Reset loss gradients from previous batch iteration
+            loss = calc_batch_loss(input_batch, target_batch, model, device)
+            loss.backward() # Calculate loss gradients
+            optimizer.step() # Update model weights using loss gradients
+            examples_seen += input_batch.shape[0] # New: track examples instead of tokens
+            global_step += 1
 
+            # Optional evaluation step
             if global_step % eval_freq == 0:
-                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
-            train_losses.append(train_loss)
-            val_losses.append(val_loss)
-            print(f"Ep {epoch + 1} (Step {global_step:06d}) : "
-                  f"Train Loss {train_loss:.3f}, "
-                  f"Val loss {val_loss:.3f}")
-        
-            train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=eval_iter)
-            val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=eval_iter)
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
 
-            print(f"Training Accuracy: {train_accuracy*100:.2f}%  | ", end="")
-            print(f"Validation Accuracy: {val_accuracy*100:.2f}%")
-            train_accs.append(train_accuracy)
-            val_accs.append(val_accuracy)
+        # Calculate accuracy after each epoch
+        train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=eval_iter)
+        val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=eval_iter)
+        print(f"Training accuracy: {train_accuracy*100:.2f}% | ", end="")
+        print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
 
     return train_losses, val_losses, train_accs, val_accs, examples_seen
 
@@ -124,25 +126,26 @@ def plot_values(epochs_seen, examples_seen, train_values, val_values, label="los
     plt.show()
 
 
-def classify_review(text, model, tokenizer, device, max_length=120, pad_token_id=50266):
-    model.to(device)
+def classify_review(text, model, tokenizer, device, max_length=None, pad_token_id=50256):
     model.eval()
 
+    # Prepare inputs to the model
     input_ids = tokenizer.encode(text)
- 
-    # using th GPT2 Model head from transformers lib
     supported_context_length = model.transformer.wpe.weight.shape[0]
+    # Note: In the book, this was originally written as pos_emb.weight.shape[1] by mistake
+    # It didn't break the code but would have caused unnecessary truncation (to 768 instead of 1024)
 
-    # truncate to only allow max context length
+    # Truncate sequences if they too long
     input_ids = input_ids[:min(max_length, supported_context_length)]
 
-
-    # pad the tokens if input is shorter
+    # Pad sequences to the longest sequence
     input_ids += [pad_token_id] * (max_length - len(input_ids))
-    input_tensor = torch.tensor(input_ids, device=device).unsqueeze(0)
+    input_tensor = torch.tensor(input_ids, device=device).unsqueeze(0) # add batch dimension
 
+    # Model inference
     with torch.no_grad():
-        logits = model(input_tensor).logits[:, -1, :]
+        logits = model(input_tensor).logits[:, -1, :]  # Logits of the last output token
     predicted_label = torch.argmax(logits, dim=-1).item()
 
+    # Return the classified result
     return "spam" if predicted_label == 1 else "not spam"
