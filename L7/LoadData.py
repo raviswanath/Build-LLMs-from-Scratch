60f3a8543b1ad0695765682cbe7a790e1aeb3487
Aadithya Viswanath Ramasubramaniam <aadithya.viswanath@gmail.com>
2025-01-30 07:02:16 -0500

    Update LoadData.py

--- L7/LoadData.py
+++ L7/LoadData.py
@@ -1,30 +1,56 @@
-import json
-import os
-import urllib.request
+import os 
+import sys 
+import torch 
+# Add the root directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from L7.LoadData import data
+from L7.UtilFunctions import format_input, InstructionDataset, custom_collate
+from torch.utils.data import DataLoader
+import tiktoken 
+from functools import partial
 
+tokenizer = tiktoken.get_encoding("gpt2")
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
-def download_and_load_file(file_path, url):
+model_input = format_input(data[50])
+desired_response = f"\n\n### Response:\n{data[50]['output']}"
+print(model_input + desired_response)
 
-    if not os.path.exists(file_path):
-        with urllib.request.urlopen(url) as response:
-            text_data = response.read().decode("utf-8")
-        with open(file_path, "w", encoding="utf-8") as file:
-            file.write(text_data)
-    else:
-        with open(file_path, "r", encoding="utf-8") as file:
-            text_data = file.read()
+train_len = int(len(data) * 0.85) 
+test_len = int(len(data) * 0.1) 
+val_len = train_len - test_len
 
-    with open(file_path, "r", encoding="utf-8") as file:
-        data = json.load(file)
+train_data = data[:train_len]
+test_data = data[train_len : train_len + test_len]
+val_data = data[train_len + test_len :]
 
-    return data
+print("Training set length:", len(train_data))
+print("Validation set length:", len(val_data))
+print("Test set length:", len(test_data))
 
-
-file_path = "instruction-data.json"
-url = (
-    "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch"
-    "/main/ch07/01_main-chapter-code/instruction-data.json"
+customized_collate_function = partial(
+    custom_collate, 
+    device=device, 
+    allowed_max_length=1024
 )
 
-data = download_and_load_file(file_path, url)
-print("Number of entries:", len(data))
+num_workers = 0
+batch_size = 8
+
+train_dataset = InstructionDataset(train_data, tokenizer=tokenizer)
+val_dataset = InstructionDataset(val_data, tokenizer=tokenizer)
+test_dataset = InstructionDataset(test_data, tokenizer=tokenizer)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=batch_size,
+    collate_fn=customized_collate_function, 
+    shuffle=True,
+    drop_last=True, 
+    num_workers=num_workers
+)
+
+
+print("Train loader:")
+for inputs, targets in train_loader:
+    print(inputs.shape, targets.shape)
